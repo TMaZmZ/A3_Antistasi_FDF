f13d9a769d7dfe0674cc2f68dfa42f5c80b85fc0
Punakuono1234 <130596011+Punakuono1234@users.noreply.github.com>
2023-04-13 18:15:57 +0300

    First rough test version

---
+++ a3_antistasi_fdf/addons/core/Includes/mod_name.hpp
@@ -0,0 +1,4 @@
+#define MODFOLDER a3_antistasi_fdf
+#ifndef PREFIX
+    #define PREFIX a3_antistasi_fdf
+#endif
---
+++ a3_antistasi_fdf/addons/templates/script_component.hpp
@@ -0,0 +1,2 @@
+#define COMPONENT templates
+#include "\x\a3_antistasi_fdf\addons\core\Includes\script_mod.hpp"
