141028b4bfae0aa22777b1ce7aab1e834786ed39
Punakuono1234 <130596011+Punakuono1234@users.noreply.github.com>
2023-04-13 22:39:36 +0300

    Lipun korjaus

--- a3_antistasi_fdf/addons/templates/Templates/Templates.hpp
+++ a3_antistasi_fdf/addons/templates/Templates/Templates.hpp
@@ -5,7 +5,7 @@ class Templates {
     {
         basepath = QPATHTOFOLDER(Templates\Vanilla); //the path to the folder the template is located in, this translates to "\x\A3AE\addons\templates\Templates\Vanilla"
         side = "Reb"; //the side the faction defaults to, one of the following: Inv, Occ, Reb, Civ
-        flagTexture = "\FFP_config\data\fin_flag_ca.paa"; //path to an icon to be displayed in the selector
+        flagTexture = "\FFP_config\data\flag\fin_flag_ca.paa"; //path to an icon to be displayed in the selector
         name = "Finnish Defense Forces"; //the name shown in the selector
         file = "SFP_REB_FDF"; //the template file name
         maps[] = {}; //if this template should be prioritized on any maps (case sensetive to worldName)
