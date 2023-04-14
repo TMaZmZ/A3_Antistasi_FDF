49cef5569504309fa75aa03dedccbc7171aa4c52
Punakuono1234 <130596011+Punakuono1234@users.noreply.github.com>
2023-04-14 19:27:01 +0300

    Various changes

---
+++ a3_antistasi_fdf/addons/core/CfgFunctions.hpp
@@ -0,0 +1,10 @@
+class CfgFunctions
+{
+    class A3A
+    {
+		class init {
+            file = QPATHTOFOLDER(functions\init);
+			class initVarOPFOR {};
+		};
+	};
+};
--- a3_antistasi_fdf/addons/templates/Templates/Templates.hpp
+++ a3_antistasi_fdf/addons/templates/Templates/Templates.hpp
@@ -1,14 +1,28 @@
 class Templates {
-    class Vanilla_Base; //import Vanilla_Base from A3A to use with defining a new vanilla template
 
-	class FDF : Vanilla_Base
+	class FDF_Base
     {
-        basepath = QPATHTOFOLDER(Templates\Vanilla); //the path to the folder the template is located in, this translates to "\x\A3AE\addons\templates\Templates\Vanilla"
+        basepath = QPATHTOFOLDER(Templates\FDF); //the path to the folder the template is located in, this translates to "\x\A3AE\addons\templates\Templates\Vanilla"
         side = "Reb"; //the side the faction defaults to, one of the following: Inv, Occ, Reb, Civ
         flagTexture = "\FFP_config\data\flag\fin_flag_ca.paa"; //path to an icon to be displayed in the selector
-        name = "Finnish Defense Forces"; //the name shown in the selector
-        file = "SFP_REB_FDF"; //the template file name
-        maps[] = {}; //if this template should be prioritized on any maps (case sensetive to worldName)
-        climate[] = {"arid", "arctic", "temperate"}; //climate that the template is meant for
+       
     };
+	
+	class FDF_Summer : FDF_Base
+	{
+		name = "FDF Summer"; //the name shown in the selector
+        file = "SFP_REB_FDF_Summer"; //the template file name
+        maps[] = {}; //if this template should be prioritized on any maps (case sensetive to worldName)
+        climate[] = {"arid", "temperate"}; //climate that the template is meant for
+	};
+	
+	/*
+	class FDF_Winter : FDF_Base //Placeholder
+	{
+		name = "FDF Winter"; //the name shown in the selector
+        file = "SFP_REB_FDF_Winter"; //the template file name
+        maps[] = {}; //if this template should be prioritized on any maps (case sensetive to worldName)
+        climate[] = {"arctic"}; //climate that the template is meant for
+	};
+	*/
 };
